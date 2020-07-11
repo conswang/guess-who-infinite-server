28033208755bc923ad5dd1faf728debdf6489d80
Cons Wang <conswang@seas.upenn.edu>
2020-07-11 09:20:27 -0400

    heroku fix

--- routes/category.js
+++ routes/category.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
-const Category = require('../models/Category');
+const Category = require('../models/category');
 
 router.get('/', (req, res) => {
     Category.find()
