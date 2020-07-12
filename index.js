7be8ce80ccdca61987b5dc3b8b1ef09b79703d49
Cons Wang <conswang@seas.upenn.edu>
2020-07-12 09:24:27 -0400

    error checking so server doesn't crash

--- index.js
+++ index.js
@@ -35,6 +35,8 @@ const roomToGameMap = new Map();
 
 io.on('connection', client => {
 
+  console.log(`New client connected with id ${client.id}`)
+
   function getGameByClient(client) {
     const rooms = Object.keys(client.rooms);
     let foundRoom;
@@ -69,8 +71,11 @@ io.on('connection', client => {
       // tell both players game has started
       io.in(room).emit('startedGame');
 
+      console.log(`JOIN GAME: game has started`);
+
     } else {
       // TODO: emit error to client: Join Code is invalid
+      console.log('JOIN GAME: invalid join code - player did not join.');
     }
 
     console.log(roomToGameMap);
@@ -78,44 +83,65 @@ io.on('connection', client => {
 
   client.on('selectCard', function (cardIdx) {
     let game = getGameByClient(client);
-    const canStartQuestions = game.selectCard(client.id, cardIdx);
+    if (game) {
+      const canStartQuestions = game.selectCard(client.id, cardIdx);
 
-    if (canStartQuestions) {
-      client.emit('startedQuestions');
+      if (canStartQuestions) {
+        client.emit('startedQuestions');
+        console.log('SELECT CARD: Both players selected cards - can start questions');
+      } else {
+        client.emit('waitingForSelection');
+        console.log('SELECT CARD: One player selected card - waiting for other player before starting questions');
+      }
+      console.log(roomToGameMap);
     } else {
-      client.emit('waitingForSelection');
+      console.log(`SELECT CARD: Invalid client id ${client.id}`)
     }
-
-    console.log(roomToGameMap);
   });
 
   // emits 'ask' | 'answer' back to client
   client.on('getTurn', function () {
     let game = getGameByClient(client);
-    if (client.id === game.player1.id) {
-      client.emit('turn', 'ask');
+    if (game) {
+      if (client.id === game.player1.id) {
+        client.emit('turn', 'ask');
+      } else {
+        client.emit('turn', 'answer');
+      }
     } else {
-      client.emit('turn', 'answer');
+      console.log(`GET TURN: Invalid client id ${client.id}`)
     }
   });
 
   client.on('ask', function (question) {
-    let room = getGameByClient(client).room;
-    // emit question to other player only
-    client.to(room).emit('playerAsked', question);
+    let game = getGameByClient(client);
+    if (game) {
+      // emit question to other player only
+      client.to(game.room).emit('playerAsked', question);
+    } else {
+      console.log(`ASK: Invalid client id ${client.id}`)
+    }
   });
   
   client.on('answer', function (isTrue) {
-    let room = getGameByClient(client).room;
-    // emit answer to other player only
-    client.to(room).emit('playerAnswered', isTrue);
+    let game = getGameByClient(client);
+    if (game) {
+      // emit answer to other player only
+      client.to(game.room).emit('playerAnswered', isTrue);
+    } else {
+      console.log(`ANSWER: Invalid client id ${client.id}`)
+    }
   });
 
   client.on('guess', function (cardIdx) {
     let game = getGameByClient(client);
-    let result = game.guess(client.id, cardIdx);
-    // emit result to both players in form { winner: 'bob'; guess: 'correct' }
-    io.in(game.room).emit('gameEnded', result);
+    if (game) {
+      let result = game.guess(client.id, cardIdx);
+      // emit result to both players in form { winner: 'bob'; guess: 'correct' }
+      io.in(game.room).emit('gameEnded', result);
+    } else {
+      console.log(`GUESS: Invalid client id ${client.id}`)
+    }
   });
 
 });
